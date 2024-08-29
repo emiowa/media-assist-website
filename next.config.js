831091846f15f6270a88b73a591b756858e65dc4
Owashi.E <emiowa@hotmail.com>
2024-08-29 15:58:33 +0900

    Adding new configurations to next.config.js and package.json to run build

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
-module.exports = {
-  i18n: {
-    defaultLocale: 'jp',
-    locales: ['jp', 'en', 'sp'],
-  },
-};
-
-
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
 
 const nextConfig = {
+  i18n: {
+    defaultLocale: 'jp',
+    locales: ['jp', 'en', 'sp'],
+  },
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  output: 'export',
 };
+
 module.exports = withMDX(nextConfig);
